3fc96f9abb585f24d377c06ca3b50589864acdf0
Adam Hernandez <ahernandez411@gmail.com>
2025-09-30 08:30:43 -0600

    add schedule

--- src/lib/gag_reader.py
+++ src/lib/gag_reader.py
@@ -1,28 +1,49 @@
 import requests
 
+from datetime import datetime
 from lib.env_reader import EnvReader
 from lib.file_helper import FileHelper
 
 class GAGReader:
     # https://api.joshlei.com/
-    def __init__(self, envs: EnvReader):
+    def __init__(self, envs: EnvReader, notify_in_stock: dict):
         self.envs = envs
+
+        self.notify_egg_stock = notify_in_stock.get("egg_stock", [])
+        self.notify_gear_stock = notify_in_stock.get("gear_stock", [])
+        self.notify_seed_stock = notify_in_stock.get("seed_stock", [])
 
         self.headers = {
             "accept": "application/json",
             "jstudio-key": self.envs.api_key
         }
+
         self.base_api = "https://api.joshlei.com/v2/growagarden"
 
 
     def get_items_in_stock(self) -> dict:
+        print("Check if items are in stock")
+
         api_url = f"{self.base_api}/stock"
         response = requests.get(api_url, headers=self.headers)
         results = response.json()
 
         FileHelper.save_json(FileHelper.DIR_TEMP, "items-in-stock.json", results)
 
-        return results
+        in_stock_items = {}
+        if self.notify_egg_stock:
+            self._add_if_in_stock(in_stock_items, results, "egg_stock", self.notify_egg_stock)
+
+        if self.notify_gear_stock:
+            self._add_if_in_stock(in_stock_items, results, "gear_stock", self.notify_gear_stock)
+
+        if self.notify_seed_stock:
+            self._add_if_in_stock(in_stock_items, results, "seed_stock", self.notify_seed_stock)
+
+        if not in_stock_items:
+            print("Zero watched items in stock!")
+
+        return in_stock_items
 
 
     def get_items_all(self) -> dict:
@@ -33,3 +54,31 @@ class GAGReader:
         FileHelper.save_json(FileHelper.DIR_TEMP, "items-all.json", results)
 
         return results
+
+
+    def _add_if_in_stock(self, in_stock_items: dict, current_stock: dict, stock_category: str, notify_stocks: list):
+        print(f"Look in {stock_category}")
+
+        current_egg_stock = current_stock[stock_category]
+        for current in current_egg_stock:
+            name = current.get("display_name")
+
+            if name.lower() in notify_stocks:
+                print(f"{name} is in stock!")
+                if stock_category not in in_stock_items:
+                    in_stock_items[stock_category] = {}
+
+                quantity = current.get("quantity")
+                end_time = current.get("Date_End")
+
+                in_stock_items[stock_category][name] = {
+                    "name": name,
+                    "quantity": quantity,
+                    "end-time": end_time,
+                }
+
+
+    def _convert_date_str_to_friendly_time(self, date_str: str) -> str:
+        dt = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.000Z")
+        formatted = dt.strftime("%Y-%m-%d %I:%M %p")
+        return formatted
--- src/main.py
+++ src/main.py
@@ -8,18 +8,49 @@ from lib.ntfy_handler import NtfyHandler
 class Main:
     def __init__(self):
         envs = EnvReader()
-        self.gag = GAGReader(envs)
+        notify_in_stock = FileHelper.load_json(FileHelper.DIR_FILES, "notify-in-stock.json")
+        self.gag = GAGReader(envs, notify_in_stock)
         self.ntfy = NtfyHandler(envs)
-        self.notify_in_stock = FileHelper.load_json(FileHelper.DIR_FILES, "notify-in-stock.json")
+        self.notify_in_stock = notify_in_stock
+
 
     def run(self):
+        print("----------------------------------------")
+        print("Check Stocks")
+        print("----------------------------------------")
+
+        self._show_watched_items()
+
         in_stock = self.gag.get_items_in_stock()
-        print(in_stock)
+        self._format_and_send_message(in_stock)
 
-        all = self.gag.get_items_all()
-        print(all)
 
-        self.ntfy.send_message("From Adam", in_stock.get("seed_stock"))
+    def _format_and_send_message(self, in_stock: dict):
+        if not in_stock:
+            print("There are zero watched items in stock")
+
+        else:
+            message_lines = []
+            for stock_category in in_stock:
+                message_lines.append(stock_category)
+
+                item = in_stock[stock_category]
+
+                message_lines.append(f"- Name: {item['name']}")
+                message_lines.append(f"- Quantity: {item['quantity']}")
+                message_lines.append(f"- Leaves at: {item['end-time']}")
+
+            message_str = os.linesep.join(message_lines)
+            self.ntfy.send_message("In Stock Items", message_str)
+
+
+    def _show_watched_items(self):
+        print("Looking for the following items:")
+        for stock_category in self.notify_in_stock:
+            print("")
+            print(stock_category)
+            for item in self.notify_in_stock[stock_category]:
+                print(f"- {item}")
 
 
 if __name__ == "__main__":
