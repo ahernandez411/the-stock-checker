3fc95007679269f684d0f6b1f85a2da04d2c7454
Adam Hernandez <adam.hernandez5@towerswatson.com>
2025-09-30 07:33:39 -0600

    simplify

--- src/lib/env_reader.py
+++ src/lib/env_reader.py
@@ -4,4 +4,3 @@ class EnvReader:
     def __init__(self):
         self.ntfy_name = os.getenv("NTFY_NAME")
         self.api_key = os.getenv("JSTUDIO_API_KEY")
-        self.secret_key = os.getenv("JSTUDIO_SECRET_KEY")
