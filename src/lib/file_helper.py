b34d2e673840fe1d3956059334b89f0a79c3acdb
Adam Hernandez <adam.hernandez5@wtwco.com>
2025-10-06 13:22:50 -0600

    change

--- src/lib/file_helper.py
+++ src/lib/file_helper.py
@@ -16,7 +16,7 @@ class FileHelper:
     FILENAME_NOTIFY_IN_STOCK = "notify-in-stock.json"
     FILENAME_RARITY_LEVELS = "rarity-levels.json"
     FILENAME_TYPE_RARITY_NAMES = "type-rarity-names.json"
-    FILENAME_RARITIES_MD = "wiki/Home.md"
+    FILENAME_RARITIES_MD = os.path.join(DIR_WIKI, "Home.md")
 
     @staticmethod
     def save_json(directory: str, filename: str, results: object):
