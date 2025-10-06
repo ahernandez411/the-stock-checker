9c459c4627a0069623aae11176e3bc323c672b29
Adam Hernandez <adam.hernandez5@wtwco.com>
2025-10-06 14:03:50 -0600

    closer

--- src/main.py
+++ src/main.py
@@ -56,24 +56,26 @@ class Main:
 
                 category_md_list.append("")
                 category_md_list.append(colors)
-                category_md_list.append(f"{rarity}")
-                category_md_list.append(colors)
-                category_md_list.append("")
 
+                category_md_list.append("")
+                category_md_list.append(f"{rarity}")
+
+                category_md_list.append("")
+                category_md_list.append(colors)
 
                 item_names = sort_levels[sort_level]
                 for item_name in item_names:
                     item = item_names[item_name]
 
-                    category_md_list.append(f"- **{item_name}**")
+                    category_md_list.append(f"- **{rarity}** -> **{item_name}**")
                     category_md_list.append("")
-
-                    icon = item.get("icon")
-                    category_md_list.append(f'    - <img src="{icon}" alt="{item_name}" width="100" />')
 
                     description = item.get("description")
                     if description:
                         category_md_list.append(f"    - {description}")
+
+                    icon = item.get("icon")
+                    category_md_list.append(f'    - <img src="{icon}" alt="{item_name}" width="100" />')
 
                     last_seen = item.get("last-seen")
                     if last_seen != self.last_seen_unknown:
