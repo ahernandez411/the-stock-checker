5fb2575341011fc3755b3912916f58d232026a71
Adam Hernandez <adam.hernandez5@wtwco.com>
2025-10-06 13:15:42 -0600

    add wiki

--- src/lib/file_helper.py
+++ src/lib/file_helper.py
@@ -2,6 +2,9 @@ import json
 import os
 
 class FileHelper:
+    DIR_WIKI = "wiki"
+    os.makedirs(DIR_WIKI, exist_ok=True)
+
     DIR_TEMP = "temp"
     os.makedirs(DIR_TEMP, exist_ok=True)
 
@@ -13,7 +16,7 @@ class FileHelper:
     FILENAME_NOTIFY_IN_STOCK = "notify-in-stock.json"
     FILENAME_RARITY_LEVELS = "rarity-levels.json"
     FILENAME_TYPE_RARITY_NAMES = "type-rarity-names.json"
-    FILENAME_RARITIES_MD = "README.md"
+    FILENAME_RARITIES_MD = "wiki/Home.md"
 
     @staticmethod
     def save_json(directory: str, filename: str, results: object):
